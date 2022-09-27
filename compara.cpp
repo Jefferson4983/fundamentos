46fe7c382eb650fe6f62489933582093a12df96e
Jefferson4983 <jefferson.caicedo.arturo@utelvt.edu.ec>
2022-09-26 23:01:23 -0500

    Programade compara.cpp

---
+++ compara.cpp
@@ -0,0 +1,27 @@
+#include<iostream>
+using namespace std;
+int main  ()
+{
+	float x,j;
+	cout<< " ingrese x j: " ; cin>>x>>j;
+	if (x > j)
+	{
+	  cout<<x<< " es mayor j " <<j<<endl;
+
+
+	} else  {
+	
+
+	  cout<<j<< " es mayor x  " <<x<<endl;
+
+
+	}
+
+	return  0 ;
+
+
+
+
+
+
+}

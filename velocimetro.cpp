45fb7bce67c43f2d55610edf5a0a02bdaeabd2d8
Jefferson4983 <jefferson.caicedo.arturo@utelvt.edu.ec>
2022-09-26 22:31:41 -0500

    Programa de velocimetro.cpp

---
+++ velocimetro.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std; 
+
+int  main()
+{
+	float x,j,c;
+	cout<< " Ingrese la distancia (en km) x= " ;cin>>x;
+	cout<< " Ingrese el tiempo (en segundo) t= " ;cin>>j;
+	c=x/j;
+	cout<< " La velocidad calculada es de c= " <<c<<endl<<endl;
+
+	 return  0 ;
+}

fe7493ec2b98b739c74cab9724111544cc7f073e
Jefferson4983 <jefferson.caicedo.arturo@utelvt.edu.ec>
2022-09-26 21:34:14 -0500

    Programa de suma.cpp

---
+++ suma.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+	float j,x,c;
+	cout<<"ingrese primer  numeros j=";cin>>j;
+	cout<<"ingrese el segundo numero x=";cin>>x;
+	c=j+x;
+	cout<<"El resultado es "<<j<<" + "<<x<<"="<<c<<endl;
+
+	return 0;
+}
+
